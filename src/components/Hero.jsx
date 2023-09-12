3e635f97b2b6e544cbc53c77f5744179c4ef3f33
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-09-12 22:33:00 +0530

    Working on HERO part

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,29 @@
+import React from 'react';
 import { logo } from '../assets';
 
 const Hero = () => {
-  return (   
+  return (
     <header className="w-full flex justify-center items-center flex-col">
-        <nav className="flex justify-between items-center flex-col">
-            <img src="{logo}" alt="sumz_logo" className='w-28 object-contain' />
-        </nav>
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className='w-28 object-contain' />
+        <button
+          type="button"
+          onClick={() => window.open("https://github.com/IamSudhir-Kumar")}
+          className="black_btn"
+        >Gthub</button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize articles with <br
+          className="max-md:hidden" />
+        <span className="trust_gradient">OpenAI GPT-4</span>
+        <h2 className="desc">
+          Simplify your reading with Summize, an open source article summarizer that transforms lengthy articles into clear and concise 
+          summaries
+        </h2>
+      </h1>
     </header>
   )
 }
 
-export default Hero
+export default Hero;
